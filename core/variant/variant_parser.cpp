37a16be4d38ca8f4b74685240bb97250931123f8
moondog <159832633+dog-on-moon@users.noreply.github.com>
2024-10-03 17:12:08 -0500

    Update variant_parser.cpp

--- core/variant/variant_parser.cpp
+++ core/variant/variant_parser.cpp
@@ -2245,7 +2245,6 @@ Error VariantWriter::write(const Variant &p_variant, StoreStringFunc p_store_str
 			} else {
 				List<Variant> keys;
 				dict.get_key_list(&keys);
-				keys.sort();
 
 				if (keys.is_empty()) {
 					// Avoid unnecessary line break.
