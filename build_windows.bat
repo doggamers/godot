3620d3d56604531ab05d181bd6dc3969cd68ee10
moondog <mydoglivesonthemoon@gmail.com>
2026-01-26 13:22:28 -0600

    Create build_windows.bat

---
+++ build_windows.bat
@@ -0,0 +1,3 @@
+scons platform=windows target=editor arch=x86_64 d3d12=no
+scons platform=windows target=template_release arch=x86_64 d3d12=no
+scons platform=windows target=template_debug arch=x86_64 d3d12=no
