0f8f79d0c13e312907702baac86f5c59f20d3b8d
moondog <mydoglivesonthemoon@gmail.com>
2026-03-11 16:58:45 -0500

    project: setup godot secure

--- core/io/file_access_encrypted.h
+++ core/io/file_access_encrypted.h
@@ -33,7 +33,7 @@
 #include "core/crypto/crypto_core.h"
 #include "core/io/file_access.h"
 
-#define ENCRYPTED_HEADER_MAGIC 0x43454447
+#define ENCRYPTED_HEADER_MAGIC 0x4C5A4B42  // Generated Tag: "BKZL"
 
 class FileAccessEncrypted : public FileAccess {
 	GDSOFTCLASS(FileAccessEncrypted, FileAccess);
