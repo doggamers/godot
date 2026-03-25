64ea6140ddfdf444a147883898e0099ab90ef464
moondog <mydoglivesonthemoon@gmail.com>
2026-03-24 19:11:04 -0500

    windows: i trolled myself so bad

--- build_windows_debug.bat
+++ build_windows_debug.bat
@@ -1,1 +1,1 @@
-scons platform=windows target=template_release arch=x86_64 d3d12=no use_mingw=yes production=yes debug_symbols=yes lto=full
+scons platform=windows target=template_debug arch=x86_64 d3d12=no use_mingw=yes production=yes debug_symbols=yes lto=full
