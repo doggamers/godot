e3d5aa3eb4e3f5b931171b5c4692c78a501ac526
moondog <mydoglivesonthemoon@gmail.com>
2026-08-10 16:50:30 -0500

    setup windows build batches

--- build_windows.bat
+++ build_windows.bat
@@ -1,3 +1,5 @@
-scons platform=windows target=editor arch=x86_64 production=yes
-scons platform=windows target=template_release arch=x86_64 use_mingw=yes production=yes lto=full
-scons platform=windows target=template_debug arch=x86_64 use_mingw=yes production=yes debug_symbols=yes lto=full
+scons platform=windows target=editor arch=x86_64 production=yes module_mono_enabled=yes
+scons platform=windows target=template_release arch=x86_64 use_mingw=yes production=yes lto=full module_mono_enabled=yes
+
+bin\godot.windows.editor.x86_64.mono --headless --generate-mono-glue modules/mono/glue
+python .\modules\mono\build_scripts\build_assemblies.py --godot-output-dir ./bin --push-nupkgs-local "D:\dog\MyLocalNugetSource" --godot-platform=windows
--- build_windows_release_tracy.bat
+++ build_windows_release_tracy.bat
@@ -1,1 +1,4 @@
-scons platform=windows target=template_release arch=x86_64 debug_symbols=yes profiler=tracy profiler_path=D:/GitHub/tracy
+scons platform=windows target=template_release arch=x86_64 debug_symbols=yes profiler=tracy profiler_path=D:/GitHub/tracy module_mono_enabled=yes
+
+bin\godot.windows.editor.x86_64.mono --headless --generate-mono-glue modules/mono/glue
+python .\modules\mono\build_scripts\build_assemblies.py --godot-output-dir ./bin --push-nupkgs-local "D:\dog\MyLocalNugetSource" --godot-platform=windows
